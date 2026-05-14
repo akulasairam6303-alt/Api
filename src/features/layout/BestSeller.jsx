592247ac55fdb2d03fa614dbd061becd0d591d91
Sai <akulasairam6303@gmail.com>
2026-05-14 15:29:00 +0530

    Api

--- src/features/layout/BestSeller.jsx
+++ src/features/layout/BestSeller.jsx
@@ -3,43 +3,45 @@ import { useNavigate } from "react-router-dom";
 import "../layout/BestSeller.css";
 
 export default function BestSeller() {
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
+    return (
 
-    
-    <div className="trending-wrapper">
-      
-      <div className="deal-card">
-        <div className="deal-image">
 
-          <img
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=800&auto=format&fit=crop"
-            alt="watch"
-          />
+        <div className="trending-wrapper">
+
+            <div className="deal-card">
+                <div className="deal-image">
+
+                    <img src="/images/watch.jpg" alt="watch"
+                    />
+                </div>
+
+                <div className="deal-content">
+                    <span className="tag">Best Sellers</span>
+
+                    <h2>Trending Top Deals</h2>
+
+                    <p>
+                        <p>
+                            Explore top shopping destinations for fashion, electronics, accessories,
+                            and everyday essentials. Find trending products, exclusive discounts,
+                            and premium collections all in one place with a smooth and secure
+                            shopping experience.
+                        </p>
+                    </p>
+
+                    <button onClick={() => navigate("/products")}>
+                        See all Products
+                    </button>
+                </div>
+            </div>
+
+
+
         </div>
 
-        <div className="deal-content">
-          <span className="tag">Best Sellers</span>
 
-          <h2>Trending Top Deals</h2>
 
-          <p>
-            Discover unbeatable offers on fashion and electronics.
-            Handpicked daily to bring style and savings together
-          </p>
-
-          <button onClick={() => navigate("/products")}>
-            See all Products
-            </button>
-        </div>
-      </div>
-
-  
-    
-        </div>
-
-        
-     
-  );
+    );
 }
